526ceeeda4f537ff0ac293ddf14dec574daf83f9
airflowboy <ajtwoddl9194@gmail.com>
2024-12-06 23:46:57 +0900

    spring core finish

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,9 +1,16 @@
 package com.luv2code.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor : " + this.getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,7 +1,9 @@
 package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,7 @@ public class DemoController {
 
     private Coach coach;
 
-    @Autowired
-    public void setCoach(Coach coach) {
+    public DemoController(@Qualifier("aquatic") Coach coach) {
         this.coach = coach;
     }
 
